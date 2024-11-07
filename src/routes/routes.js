1238975791bafb895889dd748a650d57e3c4af6d
Kauã Freitas <kauafreitas01@gmail.com>
2024-11-07 19:09:56 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,8 @@ const express = require( "express" );
 const companyRoute = require( "./companyRoute" );
 const userRoute = require( "./userRoute" );
 
-// Inicializando o roteador
 const router = express.Router();
 
-// Usar as rotas definidas
 router.use( "/empresas", companyRoute );
 router.use( "/usuarios", userRoute );
 
