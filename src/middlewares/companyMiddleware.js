9d8d0c84b02b6c437125c370a8486e510b305f07
Kauã Freitas <kauafreitas01@gmail.com>
2024-11-07 19:11:53 -0300

    Update companyMiddleware.js

--- src/middlewares/companyMiddleware.js
+++ src/middlewares/companyMiddleware.js
@@ -1,12 +1,13 @@
 // Middleware para validação dos dados das empresas
 const companyMiddleware = {
   validateCompanyData: ( req, res, next ) => {
+    
     const { name, registration_number } = req.body;
     if ( !name || !registration_number ) {
       return res.status( 400 ).json({ error: "Nome e numero de registro são obrigatórios" });
     }
     
-    next(); // Prosseguir para o próximo middleware ou controlador
+    next();
   },
 };
 
