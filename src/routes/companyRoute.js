9ea2b64e7ec8107eb609e52a628377e71d25ebd1
Kauã Freitas <kauafreitas01@gmail.com>
2024-11-07 19:10:18 -0300

    Update companyRoute.js

--- src/routes/companyRoute.js
+++ src/routes/companyRoute.js
@@ -2,14 +2,12 @@ const express = require( "express" );
 const companyMiddleware = require( "../middlewares/companyMiddleware" );
 const companyController = require( "../controllers/companyController" );
 
-// Inicializando o roteador
 const router = express.Router();
 
-// Definir as rotas relacionadas as empresas
-router.get( "/", companyController.getAllCompanies ); // Rota para obter todas as empresas
-router.get( "/:id", companyController.getCompanyById ); // Rota para obter uma empresa específico por ID
-router.post( "/", companyMiddleware.validateCompanyData, companyController.createCompany ); // Rota para criar uma nova empresa
-router.put( "/:id", companyMiddleware.validateCompanyData, companyController.updateCompany ); // Rota para atualizar uma empresa por ID
-router.delete( "/:id", companyController.deleteCompany ); // Rota para excluir uma empresa por ID
+router.get( "/", companyController.getAllCompanies );
+router.get( "/:id", companyController.getCompanyById );
+router.post( "/", companyMiddleware.validateCompanyData, companyController.createCompany );
+router.put( "/:id", companyMiddleware.validateCompanyData, companyController.updateCompany );
+router.delete( "/:id", companyController.deleteCompany );
 
 module.exports = router;
