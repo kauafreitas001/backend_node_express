055f27e3c4f371658352ff7c1e7b260e1fff5a48
Kauã Freitas <kauafreitas01@gmail.com>
2024-11-07 19:09:40 -0300

    Update userRoute.js

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,14 +2,12 @@ const express = require( "express" );
 const userMiddleware = require( "../middlewares/userMiddleware" );
 const userController = require( "../controllers/userController" );
 
-// Inicializando o roteador
 const router = express.Router();
 
-// Definir as rotas relacionadas aos usuários
-router.get( "/", userController.getAllUsers ); // Rota para obter todos os usuários
-router.get( "/:id", userController.getUserById ); // Rota para obter um usuário específico por ID
-router.post( "/", userMiddleware.validateUserData, userController.createUser ); // Rota para criar um novo usuário
-router.put( "/:id", userMiddleware.validateUserData, userController.updateUser ); // Rota para atualizar um usuário por ID
-router.delete( "/:id", userController.deleteUser ); // Rota para excluir um usuário por ID
+router.get( "/", userController.getAllUsers );
+router.get( "/:id", userController.getUserById );
+router.post( "/", userMiddleware.validateUserData, userController.createUser );
+router.put( "/:id", userMiddleware.validateUserData, userController.updateUser );
+router.delete( "/:id", userController.deleteUser );
 
 module.exports = router;
