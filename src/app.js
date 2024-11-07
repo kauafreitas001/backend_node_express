7f60e72bc8771b9b57efecc89864f162d0b43fbc
Kauã Freitas <kauafreitas01@gmail.com>
2024-11-07 16:00:51 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
-// Importação dos módulos necessários
 const express = require( "express" );
 const cors = require( "cors" );
 const routes = require( "./routes/routes" );
 
-// Inicializando o app com Express
 const app = express();
 
-// Middlewares
-app.use( cors() ); // Permitir conexões de qualquer origem com CORS
+app.use( cors() );
 
-app.use( express.json() ); // Interpretar JSON
+app.use( express.json() );
 
-app.use( routes ); // Importar todas as rotas da aplicação
+app.use( routes );
 
-// Exportar o app para ser utilizado no server.js
 module.exports = app;
