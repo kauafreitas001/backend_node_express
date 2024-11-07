91c75dde2b3243a62f5a21c7b6ee51b3191124d0
Kauã Freitas <kauafreitas01@gmail.com>
2024-11-07 19:15:11 -0300

    Update db.js

--- src/database/config/db.js
+++ src/database/config/db.js
@@ -1,7 +1,6 @@
 const mysql = require( "mysql2" );
-require( "dotenv" ).config(); // Carregar variáveis de ambiente
+require( "dotenv" ).config();
 
-// Configuração da conexão com o banco de dados MySQL
 const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -9,20 +8,14 @@ const dbConfig = {
   database: process.env.DB_NAME,
 };
 
-// Criação da conexão com o banco de dados
 const connection = mysql.createConnection( dbConfig );
 
-// Conectar ao banco de dados
 connection.connect( err => {
   if ( err ) {
     console.error( "Erro ao conectar ao banco de dados: ", err.message );
     process.exit( 1 );
   }
   console.log( "Conexão com o banco de dados MySQL realizada com sucesso!" );
-
-  // Importar e executar os arquivos de inicialização das tabelas
-  const runMigrations = require( "../migrations/migrate" );
-  runMigrations();
 });
 
 module.exports = connection;
