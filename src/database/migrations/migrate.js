6b9b683e72b8b553609389b0a4d03d9be8ef76e7
Kauã Freitas <kauafreitas01@gmail.com>
2024-11-07 19:14:32 -0300

    Update migrate.js

--- src/database/migrations/migrate.js
+++ src/database/migrations/migrate.js
@@ -1,13 +1,11 @@
-// Importação dos arquivos de criação de tabelas
 const companyCreateTable = require( "./companyCreateTable" );
 const userCreateTable = require( "./userCreateTable" );
 
 const runMigrations = () => {
   console.log( "Executando as migrações do banco de dados..." );
-    
-  // Executar as funções de criação de tabelas
+
   companyCreateTable();
   userCreateTable();
 };
 
-module.exports = runMigrations;
+runMigrations();
